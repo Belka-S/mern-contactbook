e334345993c48a2dc1f910007dbed1a88dcaac61
Belka-S <Belka-S@users.noreply.github.com>
2023-08-28 13:18:29 +0000

    deploy: 1be84a8cd885a223f8da2fa648b8ea29a3d8d598

---
+++ static/js/415.c59f89e6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmern_phonebook=self.webpackChunkmern_phonebook||[]).push([[415],{5415:function(e,n,o){o.r(n);var t=o(4814),u=o(3329);n.default=function(){return(0,u.jsx)(t.$,{mainTitle:"Welcome to PhoneBook!"})}}}]);
+//# sourceMappingURL=415.c59f89e6.chunk.js.map
